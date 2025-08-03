58b5b46ad93331a43444e131184b79645390835a
Georgia Dimaki <ggdimaki@gmail.com>
2025-08-03 09:17:08 -0400

    Contributing "Slides" button in jupyter notebooks

--- src/extension.ts
+++ src/extension.ts
@@ -16,11 +16,11 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Register the command that allows users to explicitly open the custom editor.
     context.subscriptions.push(vscode.commands.registerCommand('ipynb-slide-preview.open', () => {
-        const activeEditor = vscode.window.activeTextEditor;
+        const activeEditor = vscode.window.activeNotebookEditor;
 
-        if (activeEditor && activeEditor.document.uri.fsPath.endsWith('.ipynb')) {
+        if (activeEditor && activeEditor.notebook.uri.fsPath.endsWith('.ipynb')) {
             // If an .ipynb file is already open and active, open it with our custom editor.
-            vscode.commands.executeCommand('vscode.openWith', activeEditor.document.uri, 'ipynb.slidePreview');
+            vscode.commands.executeCommand('vscode.openWith', activeEditor.notebook.uri, 'ipynb.slidePreview');
         } else {
             // If no .ipynb file is active, inform the user.
             vscode.window.showWarningMessage('Please open an .ipynb file first to use the "Open with IPYNB Slide Editor" command.');
